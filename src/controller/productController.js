7b11cefac99edb19350cb0abf23600c2f9cfa8a4
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2026-04-04 20:11:43 +0800

    logic and routes for products

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -50,5 +50,79 @@ export const addProduct = async (req, res) => {
 }
 
 export const getProduct = async (req, res) => {
+    try {
 
+        const product = await Product.find();
+
+        if (product === 0) {
+            return res.status(404).json({ error: "No Product" });
+        }
+        return res.status(200).json({
+            message: "Product is here.",
+            count: product.length,
+            data: product
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error", error });
+        console.error("Error in add product:", error);
+    }
+};
+
+export const editProduct = async (req, res) => {
+    const { _id, name, description, category, price, stock, image_url } = req.body;
+        
+    try {
+        if (
+            !_id ||
+            !name ||
+            !description ||
+            !category ||
+            !price ||
+            !stock ||
+            !image_url
+        ) {
+            return res.status(400).json({ error: "Complete All Field" });
+        }
+
+        const updatedProduct = await Product.findByIdAndUpdate(
+            _id,
+            { name, description, category, price, stock, image_url },
+            { new: true, runValidators: true }
+        );
+        
+        if (!updatedProduct) {
+            return res.status(404).json({ error: "Product not found" });
+        }
+
+        return res.status(200).json({
+            message: "Product updated successfully",
+            product: updatedProduct
+        });
+
+    } catch (error) {
+        console.error("Error in edit product:", error);
+        res.status(500).json({ message: "Internal server error", error });
+    }
+};
+
+export const deleteProduct = async (req, res) => {
+    const {_id} = req.body;
+  
+    try {
+        if ( !_id ) {
+            return res.status(400).json({ error: "Product ID needed!" });
+        }
+
+        const product = await Product.findByIdAndDelete(_id);
+
+        if (!product) {
+            return res.status(404).json({ error: "Product not found" });
+        }
+
+        return res.status(200).json({ message: "Product deleted successfully", data: product });
+        
+    } catch (error) {
+        console.error("Error in delete product:", error);
+        res.status(500).json({ message: "Internal server error", error });
+    }
 };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { addProduct } from '../controller/productController.js';
+import { addProduct, deleteProduct, editProduct, getProduct } from '../controller/productController.js';
 import protect from '../middleware/authMiddleware.js';
 import { isAdmin } from '../middleware/roleCheckMiddleware.js';
 
 const router = express.Router();
 
 router.post('/add', protect, isAdmin, addProduct);
+router.get('/get', protect, getProduct);
+router.put('/edit', protect, isAdmin, editProduct);
+router.delete('/delete', protect, isAdmin, deleteProduct);
 
 export default router;
