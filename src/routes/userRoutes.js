5d08d502b593645fe3f574ee0b67f2e038d41865
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2026-04-20 00:36:57 +0800

    stable

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { getUser, loginUser, logout, registerUser } from '../controller/userController.js';
+import { getUser, getAllUsers, loginUser, logout, registerUser } from '../controller/userController.js';
 import protect from '../middleware/authMiddleware.js';
+import { isAdmin } from '../middleware/roleCheckMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/getUser',protect, getUser);
+router.get('/all', protect, isAdmin, getAllUsers);
 router.post('/logout', protect, logout);
 
 export default router;
